7c4443d43e77f12446559257db61d53a5fd6fb38
flashdin <flashdin007@gmail.com>
2022-02-08 22:38:39 +0700

    first commit

---
+++ application/src/test/java/com/dnc/springchatrabbitmq/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnc.springchatrabbitmq.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ library/src/test/java/com/dnc/springchatrabbitmq/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnc.springchatrabbitmq.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
